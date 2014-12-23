c7ba4abf1ded4d97419fdab320265ca7d2d8d501
Shaohuan Li <shaohuan.li@gmail.com>
2014-12-23 14:28:19 +0800

    Added Image Picker and Library Picker

--- Demo/ViewController.h
+++ Demo/ViewController.h
@@ -9,10 +9,12 @@
 #import <UIKit/UIKit.h>
 #import "ViSearchAPI.h"
 
-@interface ViewController : UIViewController{
+@interface ViewController : UIViewController<UINavigationControllerDelegate, UIImagePickerControllerDelegate>{
     IBOutlet UITextField *idSearchTextField;
     IBOutlet UITextField *colorSearchTextField;
     IBOutlet UITextField *uploadSearchUrlTextField;
+    IBOutlet UITextView *returnedText;
+    UIImagePickerController *imagePicker;
 }
 
 @end
