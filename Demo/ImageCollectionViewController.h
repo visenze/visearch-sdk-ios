cd09c264447a32bab5eb778aa9e676443c1b2513
Shaohuan Li <shaohuan.li@gmail.com>
2015-01-08 10:22:17 +0800

    Removed ImageCollectionViewController First

--- Demo/ImageCollectionViewController.h
+++
@@ -1,9 +0,0 @@
-//
-//  ImageCollectionViewController.h
-//  Demo
-//
-//  Created by Shaohuan on 1/7/15.
-//  Copyright (c) 2015 ViSenze. All rights reserved.
-//
-
-
--- Demo/ImageCollectionViewController.m
+++
@@ -1,9 +0,0 @@
-//
-//  ImageCollectionViewController.m
-//  Demo
-//
-//  Created by Shaohuan on 1/7/15.
-//  Copyright (c) 2015 ViSenze. All rights reserved.
-//
-
-
