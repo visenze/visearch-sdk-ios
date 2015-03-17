e5de91b010f887d67ee5f62a0fe3ebabaf63adc0
said9999 <jiangyaoxuan@gmail.com>
2015-03-17 15:40:22 +0800

    1.0.0 beta version

---
+++ ViSearchSDK/ViSearchAPISource/ViSearchClient.m
@@ -0,0 +1,96 @@
+//
+//  ViSearchClient.m
+//  ViSearch
+//
+//  Created by Yaoxuan on 3/10/15.
+//  Copyright (c) 2015 Shaohuan Li. All rights reserved.
+//
+
+#import "ViSearchClient.h"
+#import "ViSearchBasicHandler.h"
+#import "ViSearchImageUploadHandler.h"
+
+@interface ViSearchClient()<ViSearchNetWorkDelegate>
+
+@end
+
+@implementation ViSearchClient
+
++ (ViSearchClient *)sharedInstance {
+    static ViSearchClient *sharedInstance = nil;
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[ViSearchClient alloc] init];
+        sharedInstance.accessKey = @"";
+        sharedInstance.secretKey = @"";
+    });
+    
+    return sharedInstance;
+}
+
+- (NSString *)host {
+    return @"http://visearch.visenze.com";
+}
+
+#pragma mark Search API
+
+- (void)searchWithColor:(ColorSearchParams *)params
+                success:(void (^)(NSInteger statusCode, ViSearchResult *data, NSError *error)) success
+                failure:(void (^)(NSInteger statusCode, ViSearchResult *data, NSError *error)) failure
+{
+    ViSearchHandler *handler = [ViSearchBasicHandler new];
+    handler.searchType = @"colorsearch";
+    handler.delegate = self;
+    
+    [handler handleWithParams:params success:success failure:failure];
+}
+
+- (void)searchWithImageData:(UploadSearchParams *)params
+                    success:(void (^)(NSInteger statusCode, ViSearchResult *data, NSError *error)) success
+                    failure:(void (^)(NSInteger statusCode, ViSearchResult *data, NSError *error)) failure;
+{
+    ViSearchHandler *handler = [ViSearchImageUploadHandler new];
+    handler.searchType = @"uploadsearch";
+    handler.delegate = self;
+
+    [handler handleWithParams:params success:success failure:failure];
+}
+
+- (void)searchWithImageUrl:(UploadSearchParams *)params
+                   success:(void (^)(NSInteger statusCode, ViSearchResult *data, NSError *error))success
+                   failure:(void (^)(NSInteger statusCode, ViSearchResult *data, NSError *error))failure
+{
+    ViSearchHandler *handler = [ViSearchBasicHandler new];
+    handler.searchType = @"uploadsearch";
+    handler.delegate = self;
+
+    [handler handleWithParams:params success:success failure:failure];
+}
+
+- (void)searchWithImageId:(SearchParams *)params
+                  success:(void (^)(NSInteger statusCode, ViSearchResult *data, NSError *error)) success
+                  failure:(void (^)(NSInteger statusCode, ViSearchResult *data, NSError *error)) failure
+{
+    ViSearchHandler *handler = [ViSearchBasicHandler new];
+    handler.searchType = @"search";
+    handler.delegate = self;
+
+    [handler handleWithParams:params success:success failure:failure];
+}
+
+#pragma mark ViNetworkDelegate
+
+- (NSString *)getAccessKey {
+    return self.accessKey;
+}
+
+- (NSString *)getSecretKey {
+    return self.secretKey;
+}
+
+- (NSString *)getHost {
+    return self.host;
+}
+
+@end
